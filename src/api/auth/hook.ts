e241f8cd40863cccda9b7b45e0d132f1e808d880
emefienem <emefienemmichael@gmail.com>
2025-08-09 09:38:17 -0700

    changes

---
+++ src/api/auth/hook.ts
@@ -0,0 +1,33 @@
+import { prisma } from "@/lib/prisma";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { auth0 } from "@/lib/auth0";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await auth0.getSession();
+  const auth0Id = session?.user.sub;
+  const { email, secret } = req.body;
+  //   const MANAGER_ID = "fixed-manager-id";
+
+  if (req.method !== "POST") {
+    return res.status(403).json({ message: "Method not allowed" });
+  }
+  if (secret !== process.env.AUTH0_SECRET) {
+    return res.status(403).json({ message: `You must provide the secret` });
+  }
+  if (email) {
+    await prisma.user.create({
+      data: {
+        id: auth0Id,
+        email: session?.user.email!,
+        name: session?.user.name || null,
+        role: "CARE_WORKER",
+        // managerId: MANAGER_ID,
+      },
+    });
+    return res.status(200).json({
+      message: `User with email: ${email} has been created successfully!`,
+    });
+  }
+};
+
+export default handler;
