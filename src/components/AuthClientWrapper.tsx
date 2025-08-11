6dbdc3316d6a5c63bbc8a1b66d5cd796b9324203
emefienem <emefienemmichael@gmail.com>
2025-08-11 17:29:37 +0100

    change

--- src/components/AuthClientWrapper.tsx
+++ src/components/AuthClientWrapper.tsx
@@ -2,7 +2,6 @@
 
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import SyncUserToDb from "./SynUsersToDB";
-import { CareAppProvider } from "@/context/CareAppContext";
 
 export default function AuthClientWrapper({
   children,
@@ -11,10 +10,8 @@ export default function AuthClientWrapper({
 }) {
   return (
     <UserProvider>
-      {/* <CareAppProvider> */}
       <SyncUserToDb />
       {children}
-      {/* </CareAppProvider> */}
     </UserProvider>
   );
 }
